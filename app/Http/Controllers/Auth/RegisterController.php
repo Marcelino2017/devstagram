ef5723f54610b5d6b4563cda30a7d0e996706e31
Marcelino <marcelinomebe@gmail.com>
2023-11-28 17:13:20 -0500

    Validaciones de errores en el formulario

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -12,11 +13,8 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(Request $request)
+    public function store(RegisterRequest $request)
     {
-        $this->validate($request, [
-            'name' => 'required|min:5'
-        ]);
         dd($request);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,7 +24,11 @@
                         id="name"
                         name="name"
                         placeholder="Tú Nombre"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg
+                        @error('name')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('name') }}"
                     />
                     @error('name')
                         <p
@@ -46,8 +50,20 @@
                         id="username"
                         name="username"
                         placeholder="Tú Nombre de Usuario"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg
+                        @error('username')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('username') }}"
                     />
+                    @error('username')
+                        <p
+                            class="bg-red-500 text-white my-2
+                            w-full rounded-lg text-sm p-2 text-center"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label
@@ -60,8 +76,20 @@
                         id="email"
                         name="email"
                         placeholder="Tú Nombre Email de Registro"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg
+                        @error('emial')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('email') }}"
                     />
+                    @error('email')
+                        <p
+                            class="bg-red-500 text-white my-2
+                            w-full rounded-lg text-sm p-2 text-center"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label
@@ -74,8 +102,20 @@
                         id="password"
                         name="password"
                         placeholder="Password de Registro"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg
+                        @error('password')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('password') }}"
                     />
+                    @error('password')
+                        <p
+                            class="bg-red-500 text-white my-2
+                            w-full rounded-lg text-sm p-2 text-center"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label
@@ -89,8 +129,20 @@
                         type="text"
                         id="password_confirmation"
                         placeholder="Repite Tu Password"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg
+                        @error('password_confirmation')
+                            border-red-500
+                        @enderror"
+                        value="{{ old('password_confirmation') }}"
                     />
+                    @error('password_confirmation')
+                        <p
+                            class="bg-red-500 text-white my-2
+                            w-full rounded-lg text-sm p-2 text-center"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
                 </div>
                 <input
                     type="submit"
