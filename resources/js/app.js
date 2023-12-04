6a5297bdad98ba2e5e1e17a0570c4a945a8785e4
Marcelino <marcelinomebe@gmail.com>
2023-12-04 16:09:57 -0500

    Agregando imagen a Drozone si la validaicón falla tenemos una imagen

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,21 +10,34 @@ const dropzone = new Dropzone('#dropzone', {
     addRemoveLinks: true,
     dictRemoveFile: 'Borrar Archivo',
     maxFiles: 1,
-    uploadMultiple: false
+    uploadMultiple: false,
+
+    init: function () {
+        console.log(document.querySelector('[name="image"]').value);
+        if (document.querySelector('[name="image"]').value.trim()) {
+
+            const publishedImage = {};
+            publishedImage.size = 1234;
+            publishedImage.name = document.querySelector('[name="imagen"]').value;
+
+            this.emit('addedfile', publishedImage);
+            this.emit('thumbnail', publishedImage, `/uploads/${publishedImage.name}`);
+
+            publishedImage.previewElement.classList.add(
+                'dz-success',
+                'dz-complete'
+            );
+        }
+    }
+
 })
 
-dropzone.on("sending", function(file, xhr, formData){
-    console.log(formData);
+
+dropzone.on("success", function(file,response) {
+    document.querySelector('[name="image"]').value = response.image;
 })
 
-dropzone.on("success", function(file,response){
-    console.log(response);
-})
 
-dropzone.on("error", function(file, message){
-    console.log(message);
-})
-
-dropzone.on("removedfile", function(file, message){
-    console.log('Archivo Eliminado');
+dropzone.on("removedfile", function() {
+    document.querySelector('[name="image"]').value = "";
 })
