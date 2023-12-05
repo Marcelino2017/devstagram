b27f5e88fa7fb697e02c0b9750b2a954f3b2ae97
Marcelino <marcelinomebe@gmail.com>
2023-12-05 12:00:33 -0500

    Se añadio la caja de comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="container mx-auto flex">
-        <div class="md:w-1/2">
+        <div class="md:w-1/2 p-5">
             <img src="{{ asset('uploads').'/'.$post->image }}" alt="Imagen del post {{ $post->title }}">
 
             <div class="p-3">
@@ -23,8 +23,47 @@
                 </p>
             </div>
         </div>
-        <div class="md:w-1/2">
-        2
+        <div class="md:w-1/2 p-5">
+            <div class="shadow bg-white p-5 mb-5">
+                <p class="text-xl font-bold text-center mb-4">
+                    Agrega un Nuevo Comentaria
+                </p>
+
+                <form action="" method="POST">
+                    <div class="mb-5">
+                        <label
+                            for="comment"
+                            class="mb-2 block uppercase text-gray-500
+                            font-bold"
+                        >Añade un Comentario</label>
+                        <textarea
+                            id="comment"
+                            name="comment"
+                            placeholder="Agrega un Comentario"
+                            class="border p-3 w-full rounded-lg
+                            @error('comment')
+                                border-red-500
+                            @enderror"
+
+                        >{{ old('comment') }}</textarea>
+
+                        @error('comment')
+                            <p
+                                class="bg-red-500 text-white my-2
+                                w-full rounded-lg text-sm p-2 text-center"
+                            >
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <input
+                        type="submit"
+                        value="Crear Publicación"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
+                        uppercase font-bold w-full p-3 text-white rounded-lg"
+                    />
+                </form>
+            </div>
         </div>
     </div>
 @endsection
