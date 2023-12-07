7ab30f1adc6392e3c05b6c8f0942bf5267ce2187
Marcelino <marcelinomebe@gmail.com>
2023-12-07 17:29:09 -0500

    Mastrando enlace para editar el perfil del usuario

--- resources/views/dashboar.blade.php
+++ resources/views/dashboar.blade.php
@@ -11,17 +11,29 @@
                 <img src="{{ asset('img/usuario.svg') }}" alt="imagen usuario">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 mb:py-10">
-                <p class="text-gray-700 text-2xl">
-                    {{ $user->name }}
-                </p>
+                <div class="flex items-center gap-2">
+                    <p class="text-gray-700 text-2xl">
+                        {{ $user->username }}
+                    </p>
+
+                    @auth
+                        @if ($user->id === auth()->user()->id)
+                            <a href="#" class="text-gray-500 hover:text-gray-600 cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                                </svg>
+                            </a>
+                        @endif
+                    @endauth
+                </div>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
-                   0 <span class="font-normal">Segidores</span>
+                    0 <span class="font-normal">Segidores</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                   0 <span class="font-normal">Seguidos</span>
+                    0 <span class="font-normal">Seguidos</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                   0 <span class="font-normal">Posts</span>
+                    0 <span class="font-normal">Posts</span>
                 </p>
             </div>
         </div>
@@ -35,13 +47,12 @@
         @if ($posts->count())
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['post' => $post, 'user'=> $user]) }}">
-                        <img src="{{ asset('uploads').'/'.$post->image }}" alt="Imagen delñ post {{ $post->title }}">
-                    </a>
+                    <div>
+                        <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
+                            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="Imagen delñ post {{ $post->title }}">
+                        </a>
 
-                </div>
-
+                    </div>
                 @endforeach
             </div>
             <div class="my-10">
