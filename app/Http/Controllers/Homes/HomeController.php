35ca8da9a996a5b91be3b6d37ebeb0446f430f70
Marcelino <marcelinomebe@gmail.com>
2023-12-11 16:58:14 -0500

    Revisando que todo funcione bien

--- app/Http/Controllers/Homes/HomeController.php
+++ app/Http/Controllers/Homes/HomeController.php
@@ -8,12 +8,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function __invoke()
     {
         //Obtener a quines seguimos
         $ids = auth()->user()->followings->pluck('id')->toArray();
-        $posts = Post::whereIn('user_id', $ids)->paginate(20);
-        //dd($posts);
+        $posts = Post::whereIn('user_id', $ids)->latest()->paginate(20);
+
         return view('home', [
             'posts' => $posts,
         ]);
