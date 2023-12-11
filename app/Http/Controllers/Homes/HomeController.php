a8d9fa261fff83bed8311e9576459910c6831258
Marcelino <marcelinomebe@gmail.com>
2023-12-11 16:38:15 -0500

    Obteniendo los post de los usuarios que seguimos

--- app/Http/Controllers/Homes/HomeController.php
+++ app/Http/Controllers/Homes/HomeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Homes;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
+        //Obtener a quines seguimos
+        $ids = auth()->user()->followings->pluck('id')->toArray();
+        $posts = Post::whereIn('user_id', $ids)->paginate(20);
+        dd($posts);
         return view('home');
     }
 }
